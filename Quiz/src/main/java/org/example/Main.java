5bce0aca5a2800f358d060bc33aadf276a5f4192
FMiles-2006 <126501042+FMiles-2006@users.noreply.github.com>
2024-08-30 19:26:41 -0300

    Add files via upload

---
+++ Quiz/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+        List<String> respuestas1 = new ArrayList<String>();
+        respuestas1.add("Negro");
+        respuestas1.add("Verde");
+        respuestas1.add("Blanco");
+        respuestas1.add("Azul");
+        Preguntas pregunta1 = new Preguntas("De que color era el caballo de San Martin")
+                respuestas1, 3);
+        quiz.agregarpregunta(pregunta1);
+
+        pregunta1.mostrarPregunta();
+    }
+}

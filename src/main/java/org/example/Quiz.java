1e96db3e28749d09d1dc47abcbc73400a93cb9c9
Franmiles <franmiles11@gmail.com>
2024-09-02 18:15:07 -0300

    "Agregando codigo"

---
+++ src/main/java/org/example/Quiz.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+    private List<Preguntas> preguntas;
+    private int puntaje;
+
+    public Quiz() {
+        preguntas = new ArrayList<Preguntas>();
+        puntaje = 0;
+    }
+
+    public void agregarpregunta(Preguntas pregunta) {
+        preguntas.add(pregunta);
+    }
+
+    public void evaluarPregunta(int indicePregunta, int respuestaUsuario) {
+        if(preguntas.get(indicePregunta).getRespuestaCorrecta() == respuestaUsuario) {
+            puntaje++;
+        }
+    }
+
+    public void mostrarPuntaje() {
+        System.out.println("Su puntaje es: " + puntaje);
+    }
+
+    public void realizarPregunta(Preguntas pregunta) {
+        pregunta.mostrarPregunta();
+        System.out.print("Indique su respuesta: ");
+        int respuestaUsuario = Integer.parseInt(scanner.nextline());
+        evaluarPregunta(pregunta, respuestaUsuario);
+    }
+
+    public void iniciarQuiz() {
+        for (Preguntas pregunta: preguntas) {
+            realizarPregunta(pregunta);
+        }
+        mostrarPuntaje();
+    }
+}
